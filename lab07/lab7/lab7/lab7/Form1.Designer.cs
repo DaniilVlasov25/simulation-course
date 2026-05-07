3de55a79ef18f9a16a5525347751adfdcec7dc7f
Даниил Власов <zmachenko@mail.ru>
2026-05-08 00:06:29 +0700

    lab7

---
+++ lab07/lab7/lab7/lab7/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace lab7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.pictureBoxChart = new System.Windows.Forms.PictureBox();
+            this.lblStats = new System.Windows.Forms.Label();
+            this.lblInfo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxChart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblTitle.Location = new System.Drawing.Point(15, 0);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(515, 29);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Моделирование погоды (Цепь Маркова)";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblTime.Location = new System.Drawing.Point(16, 46);
+            this.lblTime.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(256, 20);
+            this.lblTime.TabIndex = 1;
+            this.lblTime.Text = "Время моделирования (дни):";
+            // 
+            // txtTime
+            // 
+            this.txtTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtTime.Location = new System.Drawing.Point(364, 40);
+            this.txtTime.Margin = new System.Windows.Forms.Padding(4);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(119, 26);
+            this.txtTime.TabIndex = 2;
+            this.txtTime.Text = "365";
+            this.txtTime.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnStart.ForeColor = System.Drawing.Color.White;
+            this.btnStart.Location = new System.Drawing.Point(551, 13);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(156, 31);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Смоделировать";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(740, 15);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(156, 29);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Сохранить CSV";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnReset.ForeColor = System.Drawing.Color.White;
+            this.btnReset.Location = new System.Drawing.Point(929, 15);
+            this.btnReset.Margin = new System.Windows.Forms.Padding(4);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(87, 29);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "Сброс";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // pictureBoxChart
+            // 
+            this.pictureBoxChart.BackColor = System.Drawing.Color.White;
+            this.pictureBoxChart.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxChart.Location = new System.Drawing.Point(33, 74);
+            this.pictureBoxChart.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBoxChart.Name = "pictureBoxChart";
+            this.pictureBoxChart.Size = new System.Drawing.Size(1026, 406);
+            this.pictureBoxChart.TabIndex = 6;
+            this.pictureBoxChart.TabStop = false;
+            // 
+            // lblStats
+            // 
+            this.lblStats.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(245)))), ((int)(((byte)(245)))));
+            this.lblStats.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblStats.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblStats.Location = new System.Drawing.Point(33, 484);
+            this.lblStats.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblStats.Name = "lblStats";
+            this.lblStats.Size = new System.Drawing.Size(1026, 302);
+            this.lblStats.TabIndex = 7;
+            this.lblStats.Text = "Статистика появится здесь после моделирования...";
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblInfo.ForeColor = System.Drawing.Color.Gray;
+            this.lblInfo.Location = new System.Drawing.Point(269, 786);
+            this.lblInfo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(542, 17);
+            this.lblInfo.TabIndex = 8;
+            this.lblInfo.Text = "Состояния: 1 - Ясно (желтый), 2 - Облачно (серый), 3 - Пасмурно (темно-серый)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1072, 812);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.lblStats);
+            this.Controls.Add(this.pictureBoxChart);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtTime);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.lblTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Лабораторная работа №7 - Цепь Маркова с непрерывным временем";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxChart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.PictureBox pictureBoxChart;
+        private System.Windows.Forms.Label lblStats;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
---
+++ lab07/lab7/lab7/lab7/Form1.cs
@@ -0,0 +1,442 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Windows.Forms;
+
+namespace lab7
+{
+    public partial class Form1 : Form
+    {
+       
+        static BigInteger M = BigInteger.Pow(2, 63);
+        static BigInteger Beta = BigInteger.Pow(2, 32) + 3;
+        static BigInteger X = DateTime.Now.Ticks;
+
+        static double NextDouble()
+        {
+            X = (Beta * X) % M;
+            return (double)X / (double)M;
+        }
+
+        static double GenerateExponential(double lambda)
+        {
+            double alpha = NextDouble();
+            if (alpha == 0) alpha = 1e-15; 
+            return -Math.Log(alpha) / lambda;
+        }
+
+        int numStates = 3;
+        string[] stateNames = { "Ясно", "Облачно", "Пасмурно" };
+        Color[] stateColors = { Color.Yellow, Color.LightGray, Color.DarkGray };
+
+        double[,] qOffDiagonal = {
+            { 0.0, 0.3, 0.1 },  
+            { 0.4, 0.0, 0.4 },  
+            { 0.1, 0.4, 0.0 }   
+        };
+
+        double[,] Q; 
+
+        List<double> eventTimes = new List<double>(); // Моменты переходов
+        List<int> stateSequence = new List<int>();    // Состояния в эти моменты
+        double[] totalTimeInState;                    // Общее время пребывания
+        int[,] transitionCounts;                      // Матрица переходов (для анализа)
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            totalTimeInState = new double[numStates];
+            transitionCounts = new int[numStates, numStates];
+
+            ComputeFullMatrix();
+        }
+
+        // Вычисление диагональных элементов: q_ii = -Σ q_ij
+        void ComputeFullMatrix()
+        {
+            Q = new double[numStates, numStates];
+            for (int i = 0; i < numStates; i++)
+            {
+                double sumOffDiag = 0;
+                for (int j = 0; j < numStates; j++)
+                {
+                    if (i != j)
+                    {
+                        Q[i, j] = qOffDiagonal[i, j];
+                        sumOffDiag += qOffDiagonal[i, j];
+                    }
+                }
+                Q[i, i] = -sumOffDiag; // Диагональный элемент!
+            }
+        }
+
+        void Simulate(double Tmax)
+        {
+            eventTimes.Clear();
+            stateSequence.Clear();
+            Array.Clear(totalTimeInState, 0, numStates);
+            Array.Clear(transitionCounts, 0, transitionCounts.Length);
+
+            //Выбор начального состояния
+            int currentState = (int)(numStates * NextDouble());
+
+            double currentTime = 0.0;
+            double lastEventTime = 0.0;
+
+            eventTimes.Add(currentTime);
+            stateSequence.Add(currentState);
+
+            //Основной цикл моделирования
+            while (currentTime < Tmax)
+            {
+                // Интенсивность выхода из текущего состояния
+                double lambda = -Q[currentState, currentState];
+
+                // Генерируем время пребывания в текущем состоянии
+                double tau = GenerateExponential(lambda);
+                currentTime += tau;
+
+                if (currentTime > Tmax)
+                {
+                    totalTimeInState[currentState] += (Tmax - lastEventTime);
+                    break;
+                }
+
+                totalTimeInState[currentState] += (currentTime - lastEventTime);
+                lastEventTime = currentTime;
+
+                double[] probs = new double[numStates];
+                double sumProbs = 0;
+
+                for (int j = 0; j < numStates; j++)
+                {
+                    if (j != currentState)
+                    {
+                        probs[j] = Q[currentState, j] / lambda;
+                        sumProbs += probs[j];
+                    }
+                }
+
+                for (int j = 0; j < numStates; j++)
+                    probs[j] /= sumProbs;
+
+                // Генерируем следующее состояние
+                double alpha = NextDouble();
+                double cumulative = 0;
+                int nextState = currentState;
+
+                for (int j = 0; j < numStates; j++)
+                {
+                    if (j != currentState)
+                    {
+                        cumulative += probs[j];
+                        if (alpha < cumulative)
+                        {
+                            nextState = j;
+                            break;
+                        }
+                    }
+                }
+
+                // Обновляем состояние и фиксируем переход
+                transitionCounts[currentState, nextState]++;
+                currentState = nextState;
+
+                eventTimes.Add(currentTime);
+                stateSequence.Add(currentState);
+            }
+        }
+        double[] GetEmpiricalDistribution()
+        {
+            double totalSimulatedTime = totalTimeInState.Sum();
+            double[] freq = new double[numStates];
+            for (int i = 0; i < numStates; i++)
+            {
+                freq[i] = totalTimeInState[i] / totalSimulatedTime;
+            }
+            return freq;
+        }
+
+        // Расчет теоретического стационарного распределения (Метод Гаусса)
+        double[] CalculateStationaryDistribution()
+        {
+            int n = numStates;
+            double[,] A = new double[n, n];
+            double[] b = new double[n];
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    A[i, j] = Q[j, i];
+                }
+                b[i] = 0;
+            }
+
+            for (int j = 0; j < n; j++)
+            {
+                A[n - 1, j] = 1.0;
+            }
+            b[n - 1] = 1.0;
+
+            return SolveGauss(A, b, n);
+        }
+
+        // Метод Гаусса для решения СЛАУ
+        double[] SolveGauss(double[,] A, double[] b, int n)
+        {
+            // Прямой ход
+            for (int k = 0; k < n; k++)
+            {
+                // Поиск ведущего элемента
+                int maxRow = k;
+                for (int i = k + 1; i < n; i++)
+                {
+                    if (Math.Abs(A[i, k]) > Math.Abs(A[maxRow, k]))
+                        maxRow = i;
+                }
+
+                // Обмен строк
+                for (int j = k; j < n; j++)
+                {
+                    double temp = A[k, j];
+                    A[k, j] = A[maxRow, j];
+                    A[maxRow, j] = temp;
+                }
+                double tempB = b[k];
+                b[k] = b[maxRow];
+                b[maxRow] = tempB;
+
+                // Исключение переменных
+                for (int i = k + 1; i < n; i++)
+                {
+                    double factor = A[i, k] / A[k, k];
+                    for (int j = k; j < n; j++)
+                    {
+                        A[i, j] -= factor * A[k, j];
+                    }
+                    b[i] -= factor * b[k];
+                }
+            }
+
+            // Обратный ход
+            double[] x = new double[n];
+            for (int i = n - 1; i >= 0; i--)
+            {
+                x[i] = b[i];
+                for (int j = i + 1; j < n; j++)
+                {
+                    x[i] -= A[i, j] * x[j];
+                }
+                x[i] /= A[i, i];
+            }
+                
+            return x;
+        }
+
+        private void DrawChart()
+        {
+            if (eventTimes.Count == 0) return;
+
+            int width = pictureBoxChart.Width;
+            int height = pictureBoxChart.Height;
+            Bitmap bmp = new Bitmap(width, height);
+
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                g.Clear(Color.White);
+
+                double T_max = eventTimes.Max();
+
+                // Отступы для осей
+                int startX = 60;
+                int endX = width - 30;
+                int startY = 30;
+                int endY = height - 40;
+
+                // Оси
+                Pen axisPen = new Pen(Color.Black, 1.5f);
+                g.DrawLine(axisPen, startX, startY, startX, endY); // Ось Y
+                g.DrawLine(axisPen, startX, endY, endX, endY);     // Ось X
+
+                // Подписи осей
+                g.DrawString("Время (дни)", new Font("Arial", 10, FontStyle.Bold), Brushes.Black, width / 2 - 30, height - 20);
+                g.DrawString("Состояние", new Font("Arial", 10, FontStyle.Bold), Brushes.Black, 5, 10);
+
+                // Разметка по оси Y (состояния)
+                int stateHeight = (endY - startY) / (numStates + 1);
+                for (int i = 0; i < numStates; i++)
+                {
+                    int y = endY - (i + 1) * stateHeight;
+                    g.DrawString(stateNames[i], new Font("Arial", 9, FontStyle.Bold), Brushes.Black, 5, y - 5);
+                }
+
+                // Промежуточные отметки на оси X (каждые 10% времени)
+                int numXDivisions = 10;
+                Pen gridPen = new Pen(Color.LightGray, 1) { DashStyle = System.Drawing.Drawing2D.DashStyle.Dot };
+
+                for (int i = 0; i <= numXDivisions; i++)
+                {
+                    double timeValue = (T_max / numXDivisions) * i;
+                    int x = startX + (int)((timeValue / T_max) * (endX - startX));
+
+                    // Вертикальная сетка
+                    if (i > 0 && i < numXDivisions)
+                    {
+                        g.DrawLine(gridPen, x, startY, x, endY);
+                    }
+
+                    // Риска на оси X
+                    g.DrawLine(Pens.Black, x, endY, x, endY + 4);
+
+                    // Подпись времени (округляем до целого)
+                    string label = Math.Round(timeValue).ToString();
+                    SizeF labelSize = g.MeasureString(label, Font);
+                    g.DrawString(label, Font, Brushes.Black, x - labelSize.Width / 2, endY + 6);
+                }
+
+                // Отрисовка траектории с ЦВЕТАМИ состояний
+                for (int k = 0; k < eventTimes.Count - 1; k++)
+                {
+                    double t1 = eventTimes[k];
+                    double t2 = eventTimes[k + 1];
+                    int state = stateSequence[k];
+
+                    int x1 = startX + (int)((t1 / T_max) * (endX - startX));
+                    int x2 = startX + (int)((t2 / T_max) * (endX - startX));
+                    int y = endY - (state + 1) * stateHeight;
+
+                    // Используем ЦВЕТ состояния (не синий!)
+                    Pen linePen = new Pen(stateColors[state], 4);
+                    g.DrawLine(linePen, x1, y, x2, y);
+
+                    // Точки переходов (красные)
+                    g.FillEllipse(Brushes.Red, x2 - 3, y - 3, 6, 6);
+                }
+
+                // Легенда цветов (справа сверху)
+                int legendX = endX - 70;
+                int legendY = startY - 20;
+                g.DrawString("Цвет состояний:", new Font("Arial", 8, FontStyle.Bold), Brushes.Black, legendX, legendY);
+
+                for (int i = 0; i < numStates; i++)
+                {
+                    int y = legendY + 20 + i * 15;
+                    g.FillRectangle(new SolidBrush(stateColors[i]), legendX, y, 15, 12);
+                    g.DrawRectangle(Pens.Black, legendX, y, 15, 12);
+                    g.DrawString(stateNames[i], new Font("Arial", 8), Brushes.Black, legendX + 20, y - 2);
+                }
+            }
+
+            pictureBoxChart.Image = bmp;
+        }
+
+        private void SaveToCSV(string path)
+        {
+            using (StreamWriter sw = new StreamWriter(path, false, System.Text.Encoding.UTF8))
+            {
+                // Заголовки
+                sw.WriteLine("Time;State;StateName");
+
+                // Данные траектории
+                for (int i = 0; i < eventTimes.Count; i++)
+                {
+                    sw.WriteLine($"{eventTimes[i]:F4};{stateSequence[i]};{stateNames[stateSequence[i]]}");
+                }
+
+                sw.WriteLine();
+                sw.WriteLine("Statistics;TotalTime;EmpiricalFreq;TheoreticalFreq;AbsError;RelError(%)");
+
+                double[] empirical = GetEmpiricalDistribution();
+                double[] theoretical = CalculateStationaryDistribution();
+
+                for (int i = 0; i < numStates; i++)
+                {
+                    double absErr = Math.Abs(empirical[i] - theoretical[i]);
+                    double relErr = (theoretical[i] != 0) ? (absErr / theoretical[i]) * 100 : 0;
+                    sw.WriteLine($"{stateNames[i]};{totalTimeInState[i]:F4};{empirical[i]:F4};{theoretical[i]:F4};{absErr:F4};{relErr:F2}");
+                }
+            }
+        }
+
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double Tmax = double.Parse(txtTime.Text, System.Globalization.CultureInfo.InvariantCulture);
+
+                // Запуск моделирования
+                Simulate(Tmax);
+
+                // Отрисовка
+                DrawChart();
+
+                // Расчет статистики
+                double[] empirical = GetEmpiricalDistribution();
+                double[] theoretical = CalculateStationaryDistribution();
+
+                // Вывод текста
+                string statsText = "=== СТАТИСТИЧЕСКАЯ ОБРАБОТКА ===\n\n";
+
+                for (int i = 0; i < numStates; i++)
+                {
+                    double relError = (theoretical[i] != 0) ? Math.Abs((empirical[i] - theoretical[i]) / theoretical[i] * 100) : 0;
+                    statsText += $"{stateNames[i]}:\n";
+                    statsText += $"  Эмпирическая частота: {empirical[i]:F3}\n";
+                    statsText += $"  Теоретическая (π):   {theoretical[i]:F3}\n";
+                    statsText += $"  Погрешность:         {relError:F1}%\n\n";
+                }
+
+                statsText += $"Общее время: {eventTimes.Max():F1} дней\n";
+                statsText += $"Количество переходов: {eventTimes.Count - 1}";
+
+                lblStats.Text = statsText;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (eventTimes.Count == 0)
+            {
+                MessageBox.Show("Сначала запустите моделирование!");
+                return;
+            }
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "CSV File|*.csv";
+            sfd.FileName = "WeatherSimulation.csv";
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    SaveToCSV(sfd.FileName);
+                    MessageBox.Show("Данные успешно сохранены!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ошибка при сохранении: " + ex.Message);
+                }
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            eventTimes.Clear();
+            stateSequence.Clear();
+            Array.Clear(totalTimeInState, 0, numStates);
+            Array.Clear(transitionCounts, 0, transitionCounts.Length);
+            pictureBoxChart.Image = null;
+            lblStats.Text = "";
+        }
+    }
+}
